46d13ec5365b0c997147ec92ea67103f26211268
Blaz <blaz.kosica@gmail.com>
2022-07-17 12:52:50 +0000

    Add random import of an interer to function, Add if statements in 'computer_choice' function and return the value

--- run.py
+++ run.py
@@ -7,7 +7,22 @@ import random
 cpu_wins = 0
 player_wins = 0 
 
+# Welcome Message
 
+hello_msg_border = "======================================================="
+hello_msg_text = "Hello! Welcome to Terminal game ROCK, PAPER, SCISSORS!"
+hello_msg_excited = "Are you ready to get started?"
+hello_msg_ready = "Because your opponent is already warming up!"
+
+
+print(hello_msg_border)
+print(hello_msg_text)
+print(hello_msg_excited)
+print(hello_msg_ready)
+print(hello_msg_border)
+
+
+# Player choice function
 def choose_weapon():
     """
     This function defines the users choice of "the weapon" and
@@ -27,3 +42,19 @@ def choose_weapon():
         choose_weapon()
     return user_choice
 
+# Computer choice function
+def computer_weapon():
+    """
+    This function defines the computer choice of "the weapon" and
+    which is picked randomly.
+    """
+    # Computer randomly picks a number in range 1-3
+    cpu_choice = random.randint(1, 3)
+    # Logig to assign value to number computer picks
+    if cpu_choice == 1:
+        cpu_choice = "r"
+    elif cpu_choice == 2:
+        cpu_choice = "p"
+    else:
+        cpu_choice = "s"
+    return cpu_choice
