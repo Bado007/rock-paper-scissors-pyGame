3d06622c58d55b35bc88491a3db8a0d51a14989c
Blaz <blaz.kosica@gmail.com>
2022-07-17 12:28:04 +0000

    Import random, assign variables for the game and first function, next is repeat logic for all combinations

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Rock Paper Scissors terminal game.
+
+import random 
+
+
+# define variables for the game 
+cpu_wins = 0
+player_wins = 0 
+
+
+def choose_weapon():
+    """
+    This function defines 
+    """
+    user_choice = input("Choose Rock, Paper, Scissors: ")
+    if user_choice in ["Rock", "rock", "R", "r"]:
+        user_choice = "r"
+
